8266f0c471e63b7311ada55a78d7698110752962
yonggyu.song <ygddabong@gmail.com>
2020-08-13 22:18:43 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // BEM class naming convertion
+    <div className="app">
+      <h1>Hello Yonggyu !! let's bulid a youtube clone 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBw44ItMR5yzKSBFk-HmM3jBJmR-ZrZg04",
+    authDomain: "clone-2d62c.firebaseapp.com",
+    databaseURL: "https://clone-2d62c.firebaseio.com",
+    projectId: "clone-2d62c",
+    storageBucket: "clone-2d62c.appspot.com",
+    messagingSenderId: "864957461348",
+    appId: "1:864957461348:web:6ae77c17e03d7d4fdec071",
+    measurementId: "G-HQR0GPSDJT"
+  };
