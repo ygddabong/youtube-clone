45c4ef2c01e6d7197787080c9d0242101a3f8a1b
yonggyu.song <ygddabong@gmail.com>
2020-08-16 21:52:45 +0900

    add RecommendVideos and VideoCard

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,11 +1,105 @@
 import React from 'react';
+import VideoCard from './VideoCard.js';
 import "./RecommendedVideos.css"
+
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-        <h2> i am RecommendedVideos </h2>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        <VideoCard
+          title="Plant App - Flutter UI - Speed Code"
+          views="70,130 Views"
+          timestamp="2020. 6. 26."
+          channelImage="https://yt3.ggpht.com/a/AATXAJzEkV2uEs372uXglnQSiNTSUxAV1kBCHhNHk5BI=s100-c-k-c0xffffffff-no-rj-mo"
+          channel="The Flutter Way"
+          image="https://img.youtube.com/vi/LN668OAUrK4/0.jpg"
+        />
+        </div>
+
     </div>
   );
 };
+
 
 export default RecommendedVideos;
